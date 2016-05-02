5a682f677d43c0b5f012a2335ee38f1b1133ee73
githabbot <m.githabbot@gmail.com>
2016-05-02 17:23:21 +0430

    Update share_contact.lua

--- plugins/share_contact.lua
+++ plugins/share_contact.lua
@@ -1,7 +1,7 @@
 do
 
 function run(msg, matches)
-send_contact(get_receiver(msg), "+1 302 300 3275", "", "ＱＵＩＣＫＧＵＡＲＤ", ok_cb, false)
+send_contact(get_receiver(msg), "+1 425 312 7492", "", "вøøмяαиge", ok_cb, false)
 end
 
 return {
