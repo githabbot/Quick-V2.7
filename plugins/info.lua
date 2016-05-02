6c919fb06087b42c37b47d670c45dd8287fc6e82
githabbot <m.githabbot@gmail.com>
2016-05-02 17:39:47 +0430

    Update info.lua

--- plugins/info.lua
+++ plugins/info.lua
@@ -1,5 +1,5 @@
 do
-local Arian = 112392827 --put your id here(BOT OWNER ID)
+local danger = 195473011 --put your id here(BOT OWNER ID)
 
 local function setrank(msg, name, value) -- setrank function
   local hash = nil
@@ -43,7 +43,7 @@ local function res_user_callback(extra, success, result) -- /info <username> fun
   local um_hash = 'msgs:'..result.id..':'..extra.chat2
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
   text = text..'تعداد پیام های فرستاده شده : '..user_info_msgs..'\n\n'
-  text = text..'@QuickGuardTEAM '
+  text = text..'@boomrange '
   send_msg(extra.receiver, text, ok_cb,  true)
   else
 	send_msg(extra.receiver, extra.user..' نام کاربری مورد نظر یافت نشد.', ok_cb, false)
@@ -82,7 +82,7 @@ local function action_by_id(extra, success, result)  -- /info <ID> function
   local um_hash = 'msgs:'..result.id..':'..extra.chat2
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
   text = text..'تعداد پیام های فرستاده شده : '..user_info_msgs..'\n\n'
-  text = text..'@nQuickGuardTEAM'
+  text = text..'n@boomrange'
   send_msg(extra.receiver, text, ok_cb,  true)
   else
   send_msg(extra.receiver, 'ایدی شخص مورد نظر در سیستم ثبت نشده است.\nاز دستور زیر استفاده کنید\n/info @username', ok_cb, false)
@@ -121,7 +121,7 @@ local function action_by_reply(extra, success, result)-- (reply) /info  function
   local um_hash = 'msgs:'..result.from.id..':'..result.to.id
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
   text = text..'تعداد پیام های فرستاده شده : '..user_info_msgs..'\n\n'
-  text = text..'@nQuickGuardTEAM'
+  text = text..'@nboomrange'
   send_msg(extra.receiver, text, ok_cb, true)
 end
 
@@ -194,7 +194,7 @@ local function run(msg, matches)
 	 text = text..'نام گروه : '..msg.to.title..'\n'
      text = text..'ایدی گروه : '..msg.to.id
     end
-	text = text..'\n\n@QuickGuardTEAM'
+	text = text..'\n\n@boomrange'
     return send_msg(receiver, text, ok_cb, true)
     end
   end
@@ -231,5 +231,3 @@ return {
 }
 
 end
-
--- @BossTGch
