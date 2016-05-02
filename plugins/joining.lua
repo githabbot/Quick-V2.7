e5427a674a11b6eb90cd07b86ccc4e24b534a3e1
githabbot <m.githabbot@gmail.com>
2016-05-02 17:28:04 +0430

    Update joining.lua

--- plugins/joining.lua
+++ plugins/joining.lua
@@ -1,12 +1,12 @@
 local function run(msg, matches)
     if matches[1] == "support" then
  local user = "user#id"..msg.from.id
- local fachat = "chat#id"..106191092
+ local fachat = "chat#id"..118285465
  chat_add_user(fachat, user, ok_cb, false)
     end
     if matches[1] == "ساپورت" then
  local user = "user#id"..msg.from.id
- local devchat = "chat#id"..106191092
+ local devchat = "chat#id"..118285465
  chat_add_user(devchat, user, ok_cb, false)
     end
 end
