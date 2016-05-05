c07e77ef4c25a1c0643650133323fcf3605c73c2
githabbot <m.githabbot@gmail.com>
2016-05-05 23:55:35 +0430

    Create tosuper.lua

---
+++ plugins/tosuper.lua
@@ -0,0 +1,14 @@
++local function run(msg, matches)
+ +--create by @xXDangerXx
+ +   if msg.to.type == 'chat' and is_momod then
+ +chat_upgrade('chat#id'..msg.to.id, ok_cb, false)
+ +     return "Chat Upgraded to SuperGroups"
+ +   end
+ +end
+ + --create by RoyalTeam ID CHANNEL : @xXDangerXx
+ + return {
+ +   patterns = {
+ +"^[!/#](tosuper)$",
+ +  }, 
+ +   run = run 
+ + } 
